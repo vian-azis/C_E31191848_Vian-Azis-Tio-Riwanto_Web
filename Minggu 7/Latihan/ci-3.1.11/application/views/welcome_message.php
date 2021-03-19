d290a2923b73f670690949739b60b7aa8438f515
Vian Azis <59288339+VianTio@users.noreply.github.com>
2021-03-19 23:53:51 +0700

    Update Tampilan Utama

--- Minggu 7/Latihan/ci-3.1.11/application/views/welcome_message.php
+++ Minggu 7/Latihan/ci-3.1.11/application/views/welcome_message.php
@@ -1,89 +1,123 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<style type="text/css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+    <title>E31191848 - Vian Azis Tio Riwanto</title>
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+        
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+        
+        main>.container {
+            padding: 60px 15px 0;
+        }
+    </style>
 </head>
-<body>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+<body class="d-flex flex-column h-100">
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+    <header>
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-secondary">
+            <div class="container-fluid mx-5">
+                <a class="navbar-brand">Pemrograman Web Framework</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="<?= base_url() ?>">Home</a>
+                        </li>
+						<li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>/Demo_view">Kode 1</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>/Demo_view2">Kode 2</a>
+                        </li>
+						<li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>/Demo_view3">Kode 3</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>/Demo_controller/">Controller</a>
+                        </li>
+						<li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>/Kode/php">Segment URI (Kode)</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>/Kode1/">Fungsi _remap() (Kode1)</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+    <!-- Begin page content -->
+    <main class="flex-shrink-0">
+        <div class="container">
+            <h1 class="mt-5">Selamat Datang</h1>
+            <p class="lead">Program Minggu 7 - Latihan</p>
+            <table>
+                <tr>
+                    <td>Nama </td>
+                    <td> : Vian Azis Tio Riwanto</td>
+                </tr>
+                <tr>
+                    <td>NIM </td>
+                    <td> : E31191848</td>
+                </tr>
+                <tr>
+                    <td>Golongan </td>
+                    <td> : C</td>
+                </tr>
+                <tr>
+                    <td>File Praktikum </td>
+                    <td> : <a class="btn" href="<?= base_url() ?>/Demo_view">Kode 1</a> | <a class="btn" href="<?= base_url() ?>/Demo_view2">Kode 2</a> | <a class="btn" href="<?= base_url() ?>/Demo_view3">Kode 3</a> | <a class="btn" href="<?= base_url() ?>/Demo_controller/">Controller</a> | <a class="btn" href="<?= base_url() ?>/Kode/php">Segment URI (Kode)</a> | <a class="btn" href="<?= base_url() ?>/Kode1/">Fungsi _remap() (Kode1)</a></td>
+                </tr>
+            </table>
+        </div>
+    </main>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
+    <footer class="footer mt-auto py-3 d-flex bg-light fixed-bottom">
+        <div class="container d-flex w-100">
+            <span class="text-muted mx-auto">
+                Copyright &copy;
+                <script>
+                    document.write(new Date().getFullYear());
+                </script> All rights reserved by Vian Azis
+            </span>
+        </div>
+    </footer>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
+    <!-- Optional JavaScript; choose one of the two! -->
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
 </body>
+
 </html>
